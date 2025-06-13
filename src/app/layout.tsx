7b970d66bf53c7532a8c645a5f0b39baae2457aa
berhab-zakarya <zakarya.berhab@univ-tlemcen.dz>
2025-06-13 16:59:23 +0100

    update

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -1,8 +1,10 @@
 import type { Metadata } from "next";
-import { Alumni_Sans,Poppins } from "next/font/google";
+import { Alumni_Sans, Poppins } from "next/font/google";
 import "./globals.css";
 import { ThemeProvider } from "next-themes";
-import { SpeedInsights } from "@vercel/speed-insights/next"
+import { SpeedInsights } from "@vercel/speed-insights/next";
+import Head from "next/head";
+
 const alumni = Alumni_Sans({
   subsets: ["latin"],
   variable: "--font-alumni",
@@ -21,28 +23,30 @@ export const metadata: Metadata = {
     "Discover Zakarya Berhab's portfolio – a skilled Fullstack Developer from Algeria, building high-performance web and mobile applications using Next.js, Django, Flutter, and Android Studio. Founder of StudX PTM, delivering smart SaaS solutions for businesses. Explore his work, vision, and development journey.",
 };
 
-
 export default function RootLayout({
   children,
 }: Readonly<{
   children: React.ReactNode;
 }>) {
   return (
-    <html lang="en" className={`${alumni.variable} ${PoppinsFont.variable}  antialiased`}>
-      <body
-
-      >
+    <html lang="en" className={`${alumni.variable} ${PoppinsFont.variable} antialiased`}>
+      <head>
+        <link rel="apple-touch-icon" sizes="180x180" href="/apple-touch-icon.png" />
+        <link rel="icon" type="image/png" sizes="32x32" href="/favicon-32x32.png" />
+        <link rel="icon" type="image/png" sizes="16x16" href="/favicon-16x16.png" />
+        <link rel="manifest" href="/site.webmanifest" />
+        <link rel="shortcut icon" href="/favicon.ico" />
+      </head>
+      <body>
         <ThemeProvider
-            attribute="class"
-            defaultTheme="dark"
-            enableSystem
-            disableTransitionOnChange
-          >
-            {children}
-                        <SpeedInsights/>
-
-            
-          </ThemeProvider>
+          attribute="class"
+          defaultTheme="dark"
+          enableSystem
+          disableTransitionOnChange
+        >
+          {children}
+          <SpeedInsights />
+        </ThemeProvider>
       </body>
     </html>
   );
